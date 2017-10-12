d9d201a6435c7597ea90517dc3213dcf32d344b3
Soud Al-Raihan <anik041@outlook.com>
2017-10-12 14:18:17 +0600

    Basic implementation is done.

---
+++ app/src/main/java/com/interview/raihan/mycalculator/EvaluateString.java
@@ -0,0 +1,198 @@
+package com.interview.raihan.mycalculator;
+
+import java.util.Stack;
+
+/**
+ * Created by user on 11-Oct-17.
+ */
+
+public class EvaluateString {
+
+    public static String EvaluateExpression(String str)
+    {
+        double output = 0.0;
+        try {
+            output = EvaluateDouble(str);
+        }catch (Exception e)
+        {
+            return "Error";
+        }
+
+        return Double.toString(output);
+    }
+
+    public static double EvaluateDouble(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
+    }
+
+    public static int evaluate(String expression)
+    {
+        char[] tokens = expression.toCharArray();
+
+        // Stack for numbers: 'values'
+        Stack<Integer> values = new Stack<Integer>();
+
+        // Stack for Operators: 'ops'
+        Stack<Character> ops = new Stack<Character>();
+
+        for (int i = 0; i < tokens.length; i++)
+        {
+            // Current token is a whitespace, skip it
+            if (tokens[i] == ' ')
+                continue;
+
+            // Current token is a number, push it to stack for numbers
+            if (tokens[i] >= '0' && tokens[i] <= '9')
+            {
+                StringBuffer sbuf = new StringBuffer();
+                // There may be more than one digits in number
+                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
+                    sbuf.append(tokens[i++]);
+                values.push(Integer.parseInt(sbuf.toString()));
+            }
+
+            // Current token is an opening brace, push it to 'ops'
+            else if (tokens[i] == '(')
+                ops.push(tokens[i]);
+
+                // Closing brace encountered, solve entire brace
+            else if (tokens[i] == ')')
+            {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop();
+            }
+
+            // Current token is an operator.
+            else if (tokens[i] == '+' || tokens[i] == '-' ||
+                    tokens[i] == '*' || tokens[i] == '/')
+            {
+                // While top of 'ops' has same or greater precedence to current
+                // token, which is an operator. Apply operator on top of 'ops'
+                // to top two elements in values stack
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+                // Push current token to 'ops'.
+                ops.push(tokens[i]);
+            }
+        }
+
+        // Entire expression has been parsed at this point, apply remaining
+        // ops to remaining values
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+        // Top of 'values' contains result, return it
+        return values.pop();
+    }
+
+    // Returns true if 'op2' has higher or same precedence as 'op1',
+    // otherwise returns false.
+    public static boolean hasPrecedence(char op1, char op2)
+    {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+
+    // A utility method to apply an operator 'op' on operands 'a'
+    // and 'b'. Return the result.
+    public static int applyOp(char op, int b, int a)
+    {
+        switch (op)
+        {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0)
+                    throw new
+                            UnsupportedOperationException("Cannot divide by zero");
+                return a / b;
+        }
+        return 0;
+    }
+}
--- app/src/main/java/com/interview/raihan/mycalculator/MainActivity.java
+++ app/src/main/java/com/interview/raihan/mycalculator/MainActivity.java
@@ -2,12 +2,149 @@ package com.interview.raihan.mycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] _numericButtons = {R.id.button_0,R.id.button_1,R.id.button_2,R.id.button_3,R.id.button_4,R.id.button_5,R.id.button_6,R.id.button_7,R.id.button_8,R.id.button_9};
+    private int[] _operatorButtons = {R.id.button_Add,R.id.button_Subtract,R.id.button_Multiply,R.id.button_Divide,R.id.button_Braket_Open,R.id.button_Braket_Close};
+    private TextView _currentText;
+    private boolean _haveDot;
+    private boolean _haveError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        SetNumericOnClickListener();
+        SetOperationOnClickListener();
+    }
+
+    private void init()
+    {
+        this._currentText = (TextView) findViewById(R.id.CurrentText);
+        this._haveDot = false;
+        this._haveError = false;
+    }
+
+    private void SetNumericOnClickListener()
+    {
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                if(_haveError)
+                {
+                    _haveDot = false;
+                    _currentText.setText("");
+                    _haveError = false;
+                }
+                _currentText.append(button.getText());
+
+            }
+        };
+
+        for (int id : _numericButtons)
+        {
+            findViewById(id).setOnClickListener(listener);
+        }
+
+
+        //for dot check
+
+        findViewById(R.id.button_Dot).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                if(_haveError)
+                {
+                    _haveDot = false;
+                    _currentText.setText("");
+                    _haveError = false;
+                }
+                if(!_haveDot) {
+                    _currentText.append(button.getText());
+                    _haveDot = true;
+                }
+            }
+        });
+    }
+
+    private void SetOperationOnClickListener()
+    {
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                if(_haveError)
+                {
+                    _haveDot = false;
+                    _currentText.setText("");
+                    _haveError = false;
+                }
+                _currentText.append(button.getText());
+            }
+        };
+
+        for (int id : _operatorButtons)
+        {
+            findViewById(id).setOnClickListener(listener);
+        }
+
+
+        //for clear
+
+        findViewById(R.id.button_Clear).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                _currentText.setText("");
+                _haveDot = false;
+                _haveError = false;
+            }
+        });
+
+        //for back
+
+        findViewById(R.id.button_Back).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(!_haveError)
+                {
+                    Button button = (Button) v;
+                    String str = _currentText.getText().toString();
+                    if (str != null && str.length() > 0) {
+                        if(str.charAt(str.length() - 1) =='.') _haveDot = false;
+                        str = str.substring(0, str.length() - 1);
+                        _currentText.setText(str);
+                    }
+                }
+
+            }
+        });
+
+        //for equal
+
+        findViewById(R.id.button_Equal).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(!_haveError)
+                {
+                    Button button = (Button) v;
+                    String str = _currentText.getText().toString();
+                    if (str != null && str.length() > 0) {
+                        _haveDot = false;
+                        String output = EvaluateString.EvaluateExpression(str);
+                        if(output.contains("Error")) _haveError = true;
+                        _currentText.setText(output);
+                    }
+                }
+
+
+            }
+        });
     }
 }
